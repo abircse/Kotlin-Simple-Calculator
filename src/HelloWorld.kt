5a1ee79a57fffa524afdef248ec882d89fa96727
abircse <abircoxsbazar@gmail.com>
2018-08-31 16:57:49 +0600

    Kotlin Simple Calculator

---
+++ src/HelloWorld.kt
@@ -0,0 +1,58 @@
+///-----Initialize public Variable-----//
+//----Val is a keyword for write variable(Here with value)---//
+val num1 : Int = 10;
+val num2 : Int = 20;
+
+///-----Main Fuction of Kotlin where execusion start----///
+fun main(args: Array<String>)
+{
+    //---function call from outside of main body---//
+    additon();
+    subtraction();
+    multiply();
+    division();
+}
+
+//-----2 number additional fuction----///
+fun additon() {
+
+    //----variable diclaration with out value---//
+    val sum : Int;
+    sum = num1 + num2;
+    //---Print output---//
+    println("Additional Result is: "+sum);
+
+}
+
+//-----2 number additional fuction----///
+fun subtraction() {
+
+    //----variable diclaration with out value---//
+    val sum : Int;
+    sum = num1 - num2;
+    //---Print output---//
+    println("subtraction Result is: "+sum);
+
+}
+
+//-----2 number additional fuction----///
+fun multiply() {
+
+    //----variable diclaration with out value---//
+    val sum : Int;
+    sum = num1 * num2;
+    //---Print output---//
+    println("multiplication Result is: "+sum);
+
+}
+
+//-----2 number additional fuction----///
+fun division() {
+
+    //----variable diclaration with out value---//
+    val sum : Int;
+    sum = num1 / num2;
+    //---Print output---//
+    println("divisional Result is: "+sum);
+
+}
